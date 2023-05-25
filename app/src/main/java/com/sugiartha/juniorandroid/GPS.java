213b352b168e78d3c4c14787a69307fd68766cc6
Rafli Dewanto <raflidewanto97@gmail.com>
2023-05-25 21:38:10 +0700

    feat: redesign gps activity, fix bug ketika get location, mengubah warna state button outlined, menambahkan background location pada android manifest dan menambahkan icon arrow untuk navigasi back pressed

--- app/src/main/java/com/sugiartha/juniorandroid/GPS.java
+++ app/src/main/java/com/sugiartha/juniorandroid/GPS.java
@@ -3,76 +3,165 @@ package com.sugiartha.juniorandroid;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.Looper;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class GPS extends AppCompatActivity {
-    private FusedLocationProviderClient client;
-    @SuppressLint("MissingPermission")
+    // initializing
+    // FusedLocationProviderClient
+    // object
+    FusedLocationProviderClient mFusedLocationClient;
+
+    // Initializing other items
+    // from layout file
+    TextView latitudeTextView, longitudeTextView;
+    Button backArrowButton;
+    int PERMISSION_ID = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
-        requestPermission();
-        client = LocationServices.getFusedLocationProviderClient(this);
-        Button button = findViewById(R.id.getLocation);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(GPS.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-                    System.out.println("Cek Permission");
-                    return;
-                }
-                client.getLastLocation().addOnSuccessListener(GPS.this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            TextView textView = findViewById(R.id.location);
-                            textView.setText(location.toString());
-                            System.out.print("Cek Lokasi: "+location.toString());
 
-                            Log.d("My Current location", "Lat : " + location.getLatitude() + " Long : " + location.getLongitude());
-                            // Display in Toast
-                            Toast.makeText(GPS.this,
-                                    "Lat : " + location.getLatitude() + " Long : " + location.getLongitude(),
-                                    Toast.LENGTH_LONG).show();
+        latitudeTextView = findViewById(R.id.tv_latitude);
+        longitudeTextView = findViewById(R.id.tv_longitude);
+        backArrowButton = findViewById(R.id.btn_back);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        Button getLocationButton = findViewById(R.id.btn_get_location);
+        getLocationButton.setOnClickListener(v -> getLastLocation());
+
+        backArrowButton.setOnClickListener(v -> {
+            Intent i = new Intent(GPS.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        });
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            latitudeTextView.setText("Latitude: " + location.getLatitude());
+                            longitudeTextView.setText("Longitude: " + location.getLongitude());
                         }
                     }
                 });
+            } else {
+                Toast.makeText(this, "Please turn on your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
             }
-        });
-        // GET CURRENT LOCATION
-        FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocation.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null){
-                    // Do it all with location
-                    Log.d("My Current location", "Lat : " + location.getLatitude() + " Long : " + location.getLongitude());
-                    // Display in Toast
-                    Toast.makeText(GPS.this,
-                            "Lat : " + location.getLatitude() + " Long : " + location.getLongitude(),
-                            Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
     }
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
-        System.out.println("Cek Request Permission");
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitudeTextView.setText("Latitude: " + mLastLocation.getLatitude());
+            longitudeTextView.setText("Longitude: " + mLastLocation.getLongitude());
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // method to request permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // Handle permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
     }
 }
