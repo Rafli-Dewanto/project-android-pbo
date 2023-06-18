c35bd557f2e667da11c43d46ad244158ae755616
Rafli Dewanto <raflidewanto97@gmail.com>
2023-06-18 09:43:01 +0700

    chore: add setAppBar docs and add appbar to sql act

--- app/src/main/java/com/sugiartha/juniorandroid/utils/ActivityUtils.java
+++ app/src/main/java/com/sugiartha/juniorandroid/utils/ActivityUtils.java
@@ -9,6 +9,13 @@ import com.sugiartha.juniorandroid.components.DynamicAppBar;
 public class ActivityUtils {
     private ActivityUtils() {}
 
+    /**
+     * Sets up the app bar with the specified title and back button functionality.
+     * Your Appbar's id must be named = 'appbar' for this to work
+     * @param currentActivity The current activity where the app bar is located.
+     * @param targetActivity  The target activity to navigate to when the back button is clicked.
+     * @param title           The title to set on the app bar.
+     */
     public static void setAppBar(Activity currentActivity, Class<? extends Activity> targetActivity, String title) {
         DynamicAppBar appBar = currentActivity.findViewById(R.id.appbar);
         appBar.setTitle(title);
